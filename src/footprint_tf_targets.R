6221bf386d4ee76f7c6a89c0c29026db163a7c5d
caramirezal <caramirezal@ciencias.unam.mx>
2021-02-03 18:22:07 +0100

    % targets in DEGs

--- src/footprint_tf_targets.R
+++ src/footprint_tf_targets.R
@@ -4,6 +4,7 @@
 ## Dependencies
 library(dplyr)
 library(VennDetail)
+library(ggrepel)
 
 set.seed(333)
 
@@ -33,6 +34,7 @@ footprint <- read.table(
 )
 footprint <- filter(footprint,
                     progenitor_terminal_pvalue < 0.05)
+
 head(footprint)
 dim(footprint)
 
@@ -124,3 +126,70 @@ write.table(df.filtered,
             quote = FALSE,
             row.names = FALSE)
 
+######################################################################
+
+## 
+tfs.ranked <- footprint %>%
+        filter(!grepl(':|\\(', name)) %>%
+        arrange(desc(progenitor_terminal_change)) %>%
+        mutate(rank=1:n()) %>%
+        select(name, rank)
+dim(tfs.ranked)
+head(tfs.ranked)
+
+
+tfs.ranked$'n_Hofmann_DEG' <- 0
+for (i in 1:nrow(tfs.ranked)){
+        tfbs.sel <- filter(tfbs.df, tf==tfs.ranked$name[i])
+        if (nrow(tfbs.sel) > 0 ) {
+                targets <- toupper(unique(tfbs.sel$gene_name))
+                tfs.ranked$n_Hofmann_DEG[i] <- length(
+                            unique(intersect(targets,
+                                   rownames(degs)))
+                            )
+        }
+}
+tfs.ranked <- mutate(tfs.ranked, 
+                     percentage_degs=100*n_Hofmann_DEG/nrow(degs))      
+head(tfs.ranked)
+
+tfs.ranked <- arrange(tfs.ranked, desc(percentage_degs)) %>%
+        mutate(rank2=1:n()) %>%
+        mutate(highlight=ifelse(rank<30 |
+                                        name %in% 'PRDM1', 
+                                TRUE, FALSE)) 
+tfs.ranked <- mutate(tfs.ranked,
+                     tf_name=ifelse(highlight==TRUE, name, ''))
+tfs.highlight <- subset(tfs.ranked, highlight==TRUE)
+
+pdf('figures/tf_percentage_of_target_degs.pdf')
+ggplot(tfs.ranked, aes(x=rank2, 
+                   y=percentage_degs,
+                   label=tf_name)) +
+                geom_point() +
+                geom_point(data=tfs.highlight,
+                           aes(x=rank2,
+                               y=percentage_degs),
+                               color='red') +
+                geom_text_repel(max.overlaps = 100, 
+                                force = 50) +
+                xlab('Rank') +
+                ylab('% DEGs in targets') +
+                theme_bw() +
+                theme(panel.grid = element_blank())
+
+tfs.ranked %>%
+        select(rank, percentage_degs) %>%
+        ggplot(aes(x=rank, 
+                   y=percentage_degs)) +
+                geom_bar(position="stack", 
+                         stat="identity",
+                         fill='steelblue') +
+                theme_bw() +
+                theme(axis.text.x = element_blank(),
+                      axis.ticks.x = element_blank(),
+                      panel.grid = element_blank()) +
+                xlab('Rank (Footprint score)') +
+                ylab('% DEGs in targets')
+
+dev.off()
